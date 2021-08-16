f723f2376280469d0e18479e5bbedccf504aeed1
Rarysont <rarysont@gmail.com>
2021-08-16 18:21:00 -0300

    page ticketlist

--- src/components/TicketList/index.js
+++ src/components/TicketList/index.js
@@ -7,11 +7,17 @@ export function TicketList(data){
   return (
     <View style={styles.container}>
       <View style={styles.box}>
-        <View style={styles.namesTickets}>
-          <Text style={styles.containerActions}>{data.data.abr}</Text>
-          <Text style={styles.containerActions}>{data.data.name}</Text>
+        <View style={styles.boxLogo}>
+          <View style={styles.logo}></View>
         </View>
-        <Text style={styles.containerActions}>{data.data.value}</Text>
+        <View style={styles.namesTickets}>
+          <Text style={styles.abr}>{data.data.abr}</Text>
+          <Text style={styles.nameAction}>{data.data.name}</Text>
+        </View>
+        <View style={styles.namesTickets2}>
+          <Text style={styles.value}>{data.data.value}</Text>
+          <Text style={styles.nameAction}>{data.data.percent}</Text>
+        </View>
       </View>
     </View>
   );
