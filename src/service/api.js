9abb79082f4f9cafdd27d144e9082b327d1a3f8a
Rarysont <rarysont@gmail.com>
2021-10-23 22:22:11 -0300

    fix

--- src/service/api.js
+++ src/service/api.js
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-<<<<<<< Updated upstream
   baseURL: 'https://60c7-2804-431-c7f1-793f-29b1-5e2a-1fa5-5fbc.ngrok.io',
-=======
-  baseURL: 'https://a42c-2804-431-c7f0-9b34-e4d7-147e-3a7f-f5e2.ngrok.io',
->>>>>>> Stashed changes
 });
 
 export default api;
